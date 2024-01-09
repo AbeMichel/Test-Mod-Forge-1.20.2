f10dcf2c4b94055b10f377ff50889c63d8a41227
AbeMic <122504959+AbeMic@users.noreply.github.com>
2024-01-09 11:22:49 -0600

    Added loot tables, ore, recipes, and tool requirements

---
+++ src/main/java/net/abe/abesaddons/item/ModCreativeModTabs.java
@@ -0,0 +1,34 @@
+package net.abe.abesaddons.item;
+
+import net.abe.abesaddons.AbesAddons;
+import net.abe.abesaddons.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AbesAddons.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TEST_TAB = CREATIVE_MODE_TABS.register("test_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.test_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        // Where you add items to the custom tab
+                        pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
